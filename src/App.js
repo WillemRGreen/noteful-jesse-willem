f7da78d05850e86dd2e1620f1c1ad674a805549d
Willem Green <willberforce15@gmail.com>
2020-10-06 13:17:23 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Route, Link} from 'react-router-dom';
+import NoteList from 'NoteList/NoteList';
+import ExpandedNotes from 'ExpandedNotes/ExpandedNote';
+import SideBar from 'SideBar/SideBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+
+        </nav>
+        <header>
+
+        </header>
+        <main>
+
+        </main>
+
+      </div>
+    )
+  }
+
 }
 
 export default App;
